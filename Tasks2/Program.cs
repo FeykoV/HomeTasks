63996f08f2606af8381924b11b99cc2c0ab475ff
Елена <volshebnaya2393@gmail.com>
2023-01-16 22:19:20 +0500

    Задание 2

---
+++ Tasks2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+    Console.WriteLine($"Максимальное число: {numberA}");
+if (numberA == numberB)
+   Console.WriteLine($"Числа равны {numberA} = {numberB}");
+else if (numberA < numberB)
+    Console.WriteLine($"Максимальное число: {numberB}");
+
