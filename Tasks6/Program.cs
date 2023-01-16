1a32d0c359bd35e1a8662a3977e2f8407813c2ab
Елена <volshebnaya2393@gmail.com>
2023-01-16 22:28:34 +0500

    задание 6

---
+++ Tasks6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задание 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) Console.WriteLine($"Число {n} четное");
+else Console.WriteLine($"Число {n} нечетное");
