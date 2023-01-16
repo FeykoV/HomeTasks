7a34928811d7038d694706f9973bb59c381917a3
Елена <volshebnaya2393@gmail.com>
2023-01-16 22:30:47 +0500

    задание 8

---
+++ Tasks8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задание 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 2;
+while (m <= n)
+{
+    Console.Write($"{m} ");
+    m = m + 2; 
+}
