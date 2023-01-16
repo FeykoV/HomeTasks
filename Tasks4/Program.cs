5c63f8e28cf53a4a05e4de9dda48ce7d45e532b4
Елена <volshebnaya2393@gmail.com>
2023-01-16 22:23:33 +0500

    задание 4

---
+++ Tasks4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+Console.WriteLine($"Максимальное число: {max}");
