857af46acf00467e5f615888b3e4d010ba1e30a0
Елена <volshebnaya2393@gmail.com>
2023-01-17 13:34:37 +0500

    Задание 12

---
+++ Tasks12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA % numberB == 0) 
+Console.WriteLine($"Числа кратные");
+else Console.WriteLine($"Числа не кратные, остаток {numberA % numberB} ");
