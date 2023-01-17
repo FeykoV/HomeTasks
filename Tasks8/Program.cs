750dc6b90f77eb9e63e517b14610b812e9d8f958
Елена <volshebnaya2393@gmail.com>
2023-01-17 12:34:18 +0500

    Добавочка

--- Tasks8/Program.cs
+++ Tasks8/Program.cs
@@ -9,3 +9,10 @@ while (m <= n)
     Console.Write($"{m} ");
     m = m + 2; 
 }
+// Task 8
+// тоже через цикл for
+//Console.Clear();
+//Console.Write("Введите число: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//for (int i = 2; i <= n; i += 2) // (begin, condition, step)
+//    Console.Write($"{i} ");
