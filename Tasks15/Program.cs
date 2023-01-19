483745ca40061fde01a32e44ce2cf63c815b2a2e
Елена <volshebnaya2393@gmail.com>
2023-01-19 20:15:00 +0500

    Задание 15

---
+++ Tasks15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задание 3
+
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n < 6)
+    Console.WriteLine("нет"); // нет, это - рабочий день
+else if (n == 6)
+    Console.WriteLine("да"); // да - это выходной
+else 
+    Console.WriteLine("да");
