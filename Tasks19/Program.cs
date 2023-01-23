322b3b347deed0b73b6fc19fc4cebed079bd92e7
Елена <volshebnaya2393@gmail.com>
2023-01-23 22:30:16 +0500

    задание 19

---
+++ Tasks19/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 9999 || n > 99999)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n % 10 == n / 10000 && (n / 10) % 10 == (n / 1000) % 10)
+    Console.WriteLine($"{n} - это палиндром");
+else  
+    Console.WriteLine($"{n} - это не палиндром");
---
+++ TasksDop/Program.cs
@@ -0,0 +1,13 @@
+﻿// подсказка
+// вводим числа пока n не равно 0
+Console.Clear();
+int[] array = new int[100];
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (array[i] != 0)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i = i + 1;
+}
+
+Console.WriteLine(array);
