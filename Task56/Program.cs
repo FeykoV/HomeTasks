59adb8920438c1df0886e45c9136df86734af103
Елена <volshebnaya2393@gmail.com>
2023-02-09 19:22:00 +0500

    Задание 56

---
+++ Task56/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 56: 
+//Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int MinStroka = 1; int summa = 0; int min = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summa += matrix[i, j];
+        }
+        if (i == 0)
+            min = summa;
+        else if (summa < min)
+        {
+            min = summa;
+            MinStroka = i + 1;
+        }
+        summa = 0;
+    }
+    return MinStroka;
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine($"Строка с минимальной суммой элементов: {ReleaseMatrix(matrix)}");
